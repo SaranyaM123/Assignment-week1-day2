d0cd7692d08e947b6837cbf7333a7911ed1814db
SaranyaM123 <139625700+SaranyaM123@users.noreply.github.com>
2023-07-16 10:25:25 -0700

    Add files via upload
    Week1/day2

---
+++ day2/grade.java
@@ -0,0 +1,23 @@
+package week1.day2;
+
+public class grade {
+
+	public static void main(String[] args) {
+		int mark=75;
+		if(mark==35) {
+		System.out.println("GRADE E");
+		}
+		else if(mark>=50) {
+		System.out.println("GRADE D");
+		}
+		else if(mark>65) {
+		System.out.println("GRADE C");
+		}
+		else if(mark>=75) {
+		System.out.println("GRADE B");
+		}
+	else {
+		System.out.println("GRADE A");
+	}
+	}
+}
---
+++ day2/oddNumber.java
@@ -0,0 +1,20 @@
+package week1.day2;
+
+public class oddNumber {
+
+	public static void main(String[] args) 
+	{
+		int n=20;
+		for (int i=0; i<=n; i++)
+		{
+			if(i%2==1)
+			{
+			System.out.println("THESE ARE THE ODD NUMBERS" +i);	
+			}
+			
+		}
+		
+
+	}
+
+}
